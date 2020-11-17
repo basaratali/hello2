98ac0b5f118002757340b7572d388cbfbf59ccf4
Basarat Ali <basaratali@Basarats-MacBook-Pro.local>
2020-11-17 14:41:22 +0500

    first commit

---
+++ hello/RegisterViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RegisterViewController.swift
+//  hello
+//
+//  Created by Basarat Ali on 11/17/20.
+//  Copyright © 2020 Basarat Ali. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    var flagvariable = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(flagvariable)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- hello/ViewController.swift
+++ hello/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblTitle: UILabel!
+    
+    @IBOutlet weak var txtUsername: UITextField!
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func btnSubmit(_ sender: Any)
+    {
+        let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "RegisterViewControllerID") as? RegisterViewController
+        
+        vc?.flagvariable = "Lay Jao Value"
+        self.navigationController?.pushViewController(vc!, animated: true)
+    }
+    
 }
 
