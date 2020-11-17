264bb8f63240d5c317cea4a21b9f8095b553b8dc
Basarat Ali <basaratali@basarats-macbook-pro.local>
2020-11-17 13:04:28 +0500

    Initial Commit

---
+++ hello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hello
+//
+//  Created by Basarat Ali on 11/17/20.
+//  Copyright © 2020 Basarat Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
